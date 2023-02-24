735e852f065d35b7022d62f6e88b289e2ec1583e
Chase Murtaugh <chasemurtaugh@gmail.com>
2023-02-24 17:58:54 -0500

    Add btn loaders

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -11,6 +11,10 @@ module.exports = {
 
     // Button handling
     if (interaction.isButton()) {
+      // reply loader
+      await interaction.deferReply({ ephemeral: true });
+
+      // customids are set to the button as [buttonlabel]-[assigneduserid], we split them to handle which button is called and prevent other users from touching btns
       const splitArr = interaction.customId.split('-');
       const btnType = splitArr[0];
       const authUserId = splitArr[1];
@@ -19,11 +23,11 @@ module.exports = {
       // Compares the original user id from customID with whoever interacts with it, if they don't match we end method.
       // This is to prevent other users from messing with buttons not intended for them.
       if (interactorId !== authUserId) {
-        interaction.reply({ content: 'That doesn\'t belong to you!', ephemeral: true });
+        interaction.editReply({ content: 'That doesn\'t belong to you!' });
         return;
       }
 
-      interaction.reply({ content: `<@${authUserId}>, a recruiter has been notified, we will get back to you shortly.`, ephemeral:true });
+      interaction.editReply({ content:`<@${authUserId}>, a recruiter has been notified, we will get back to you shortly.` });
 
 
     }
