532d844f7079c064c124b6df35f34a862c3a8895
Chase Murtaugh <chasemurtaugh@gmail.com>
2023-02-24 18:11:07 -0500

    Getting ahead of potential btn interaction problems

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -13,6 +13,8 @@ module.exports = {
     if (interaction.isButton()) {
       // reply loader
       await interaction.deferReply({ ephemeral: true });
+      // this is a little retarded but we're bouncing embeds instead of using proper slash commands so I'll try to be clean
+      const userBtnPrefix = ['recruitment', 'ambassador', 'attache'];
 
       // customids are set to the button as [buttonlabel]-[assigneduserid], we split them to handle which button is called and prevent other users from touching btns
       const splitArr = interaction.customId.split('-');
@@ -22,7 +24,8 @@ module.exports = {
 
       // Compares the original user id from customID with whoever interacts with it, if they don't match we end method.
       // This is to prevent other users from messing with buttons not intended for them.
-      if (interactorId !== authUserId) {
+      // the includes is to only run this check on user interactable buttons and not staff ones
+      if (interactorId !== authUserId && userBtnPrefix.includes(btnType)) {
         interaction.editReply({ content: 'That doesn\'t belong to you!' });
         return;
       }
