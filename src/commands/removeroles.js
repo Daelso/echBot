dd3319e89fdacb6d9b9fb0269133f2b7fe7070e0
Chase Murtaugh <chasemurtaugh@gmail.com>
2023-02-28 20:37:26 -0500

    OOPSIE

--- src/commands/removeroles.js
+++ src/commands/removeroles.js
@@ -7,27 +7,37 @@ const adminRoleId = '769348490848632863';
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('removeroles')
-    .setDescription('Removes roles from the input user.'),
+    .setDescription('Removes active member roles from the input user in the case of switching to Warden.'),
   async execute(interaction) {
-    const targetUser = interaction.options.get('user').user.id;
+
+    try {
+      const targetUser = interaction.options.get('user').user.id;
+
+      const adminUser = await interaction.guild.members.fetch(interaction.user.id);
 
 
-    const guildTarget = await interaction.guild.members.fetch(targetUser);
-
-    const hasRole = guildTarget.roles.cache.has(adminRoleId);
+      const guildTarget = await interaction.guild.members.fetch(targetUser);
 
 
-    if (!hasRole) {
-      interaction.reply('You do not have permission to use this command!');
-      return;
+      const hasRole = adminUser.roles.cache.has(adminRoleId);
+
+
+      if (!hasRole) {
+        interaction.reply('You do not have permission to use this command!');
+        return;
+      }
+
+
+      rolesToRemove.forEach(async role => {
+        await guildTarget.roles.remove(role);
+      });
+
+      interaction.reply(`Roles successfully removed from <@${targetUser}>`);
     }
-
-
-    rolesToRemove.forEach(async role => {
-      await guildTarget.roles.remove(role);
-    });
-
-    interaction.reply(`Roles successfully removed from <@${targetUser}>`);
+    catch (error) {
+      console.log(error);
+      interaction.reply('Error, please yell at <@139984589786578944>');
+    }
 
   },
 
