18acdf083307a22dc43567f146ff328fd49ebdf9
Chase Murtaugh <chasemurtaugh@gmail.com>
2023-03-03 14:21:03 -0500

    Tweak to private channel sys

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,9 +1,12 @@
 const { Events, Collection, ChannelType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 // Collections are an extension of a Map function more or less, AKA a fresh array
+
 const voiceCollection = new Collection();
 
-const roleId = '1078779318861303838';
+
+const roleId = '769253180050636840';
+const creatorChannel = '1081294616097206352';
 
 
 module.exports = {
@@ -11,45 +14,66 @@ module.exports = {
   once: false,
   async execute(oldState, newState) {
 
-
-    if (newState.channel !== null) {
-      if (newState.channel.id === '1080998893132718130') {
-        const user = newState.member;
-
-        const hasRole = user.roles.cache.has(roleId);
-
-        if (!hasRole) return;
+    try {
 
 
-        const channel = await newState.guild.channels.create({
-          name: `${user.user.username}'s temp vc`,
-          type: ChannelType.GuildVoice,
-          parent: newState.channel.parent,
+      if (newState.channel !== null) {
+
+
+        if (newState.channel.id === creatorChannel) {
+          const user = newState.member;
+
+          const hasRole = user.roles.cache.has(roleId);
+
+          if (!hasRole) return;
+
+
+          const channel = await newState.guild.channels.create({
+            name: `${user.user.username}'s temp vc`,
+            type: ChannelType.GuildVoice,
+            parent: newState.channel.parent,
+          });
+          user.voice.setChannel(channel);
+          voiceCollection.set(user.id, channel.id);
+
+
+          const channelEmbed = new EmbedBuilder().setTitle('Echelon Channel Builder')
+            .setDescription(`<@${user.id}>, please use the buttons below to customize your channel. This channel is designed to be temporary and will automatically delete when all users leave.`)
+            .setColor('Green')
+            .setTimestamp()
+            .setThumbnail('https://www.echclan.net/img/ECHLogo.73a81d16.png')
+            .setFooter({ text: 'Wardens go away', iconURL: 'https://www.echclan.net/img/ECHLogo.73a81d16.png' });
+
+          const actionRow = new ActionRowBuilder();
+          actionRow.components.push(new ButtonBuilder().setCustomId(`changeChannelName-${channel.id}`).setLabel('✍🏻 Set Channel Name').setStyle(ButtonStyle.Secondary));
+          actionRow.components.push(new ButtonBuilder().setCustomId(`changeUserLimit-${channel.id}`).setLabel('🤼 Set User Limit').setStyle(ButtonStyle.Secondary));
+
+
+          channel.send({ embeds: [channelEmbed], components:[actionRow] });
+        }
+      }
+
+
+      if (newState.channel === null || (oldState.channel !== null && newState.channel.id !== oldState.channel.id)) {
+
+        // if user leaves vc altogether or switches to another channel we trigger a loop through our collection of temp channels held in memory
+        // any temp channels that have 0 users left will get deleted
+
+        voiceCollection.forEach(async x => {
+
+          const curChannel = await oldState.guild.channels.fetch(x);
+
+          console.log(curChannel.members.size);
+          if (curChannel.members.size === 0) {
+            curChannel.delete();
+          }
+
         });
-        user.voice.setChannel(channel);
-        voiceCollection.set(user.id, channel.id);
 
-
-        const channelEmbed = new EmbedBuilder().setTitle('Echelon Channel Builder')
-          .setDescription(`<@${user.id}>, please use the buttons below to customize your channel. This channel is designed to be temporary and will automatically delete when all users leave.`)
-          .setColor('Green')
-          .setTimestamp()
-          .setThumbnail('https://www.echclan.net/img/ECHLogo.73a81d16.png')
-          .setFooter({ text: 'Wardens go away', iconURL: 'https://www.echclan.net/img/ECHLogo.73a81d16.png' });
-
-        const actionRow = new ActionRowBuilder();
-        actionRow.components.push(new ButtonBuilder().setCustomId(`changeChannelName-${channel.id}`).setLabel('✍🏻 Set Channel Name').setStyle(ButtonStyle.Secondary));
-        actionRow.components.push(new ButtonBuilder().setCustomId(`changeUserLimit-${channel.id}`).setLabel('🤼 Set User Limit').setStyle(ButtonStyle.Secondary));
-
-
-        channel.send({ embeds: [channelEmbed], components:[actionRow] });
       }
     }
-
-
-    if (newState.channel === null) {
-      const memberCollection = oldState.channel.members;
-      if (oldState.channel.id === voiceCollection.get(newState.id) && memberCollection.size === 0) return oldState.channel.delete();
+    catch (error) {
+      console.log(error);
     }
 
 
